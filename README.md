03e56c5bc79a7ce1a75180e49cc23f9c7029f459
Manuel <manuel@getenpowered.com>
2023-04-02 22:43:19 -0300

    add optional AWS profile

--- README.md
+++ README.md
@@ -12,10 +12,9 @@ npm install
 Create a .env file based on .env.sample file.
 ```
 //.env file
-######## REQUIRED FOR RUNNING IN LOCAL OR TO DEPLOY TO AWS ######## 
-VUE_APP_API_URL=https://tn-apis.app.com/v1
-######### REQUIRED JUT FOR AWS DEPLOY. Leve them blank if you are not deployinf to AWS ######## 
-AWS_FRONTEND_PROFILE=tnprofile //Configured AWS Profile to deploy app
+VUE_APP_API_URL=https://tn-apis.app.com/v1 // REQUIRED FOR RUNNING IN LOCAL OR TO DEPLOY TO AWS
+// REQUIRED JUT FOR AWS DEPLOY. Leve them blank if you are not deploying to AWS 
+AWS_FRONTEND_PROFILE=tnprofile //Optional. Configured AWS CLI Profile to deploy app. Leave it black if you dont want to specify a profile and use the default aws cli config.
 S3_HOST_BUCKET_NAME=tn-front // AWS S3 bucket NAME to host the app as static. You dont need to create it manually
 DOMAIN_NAME=tn-front.app.com // Doimain Name of the app
 CERTIFICATE_ARN=arn:aws:acm:us-east-1:999:certificate/xxxx //AWS Certificate Managers ARN for DNS described in DOMAIN_NAME
@@ -36,8 +35,8 @@ npm run test:unit
 ```
 
 ### Deploy on AWS
-1 Install and configure AWS cli and set up an AWS profile to deploy on your AWS account.
-2 make sure you set the var AWS_FRONTEND_PROFILE in the .env file with the profile created in the previous step.
+1 Install and configure AWS cli to execute command on your AWS account. Optionally you can set up an AWS profile.
+2 If you have set up a profile in the previous step make sure you set the env var AWS_FRONTEND_PROFILE in the .env file
 3 set the S3_HOST_BUCKET_NAME in the .env file. (this bucket will be created automatically with the deploy command. You have just to declare a name)
 4 Create a DNS with the DNS provider of your preference and set it in DOMAIN_NAME in the .env file
 5 Add/Create to AWS Certificate manager (AWS Console -> AWS Certificate Manager -> Certificates) a certificate that contains the host declared in DOMAIN_NAME and set its ARN as CERTIFICATE_ARN in the .env file
--- scripts/cf-deploy.sh
+++ scripts/cf-deploy.sh
@@ -7,37 +7,43 @@ then
     source $DIR/../.env
 fi
 
+if [ -n "$AWS_FRONTEND_PROFILE" ]
+then
+    PROFILE_COMMAND="--profile=$AWS_FRONTEND_PROFILE"
+else
+    PROFILE_COMMAND=""
+fi
+
 if [ -z "$S3_HOST_BUCKET_NAME" ]
 then
     echo "No \$S3_HOST_BUCKET_NAME env value found";
     exit 1
 else
-    AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text --profile=${AWS_FRONTEND_PROFILE});
+    AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text $PROFILE_COMMAND);
 
     echo "Build stack..."
-    COMMAND="aws cloudformation deploy --template-file build/cloud-app-stack.cf.json \
+    COMMAND="aws cloudformation deploy $PROFILE_COMMAND --template-file build/cloud-app-stack.cf.json \
     --stack ${STACK_NAME:=tn-front-app} --region us-east-1 \
     --capabilities CAPABILITY_IAM \
     --parameter-overrides S3HostBucketName=${S3_HOST_BUCKET_NAME} \
-    CertificateArn=${CERTIFICATE_ARN} DomainName=$DOMAIN_NAME \
-    --profile=${AWS_FRONTEND_PROFILE}"
+    CertificateArn=${CERTIFICATE_ARN} DomainName=$DOMAIN_NAME"
     echo $COMMAND;
     $COMMAND;
 
     echo "Clear bucket..."
-    CLEAR_COMMAND="aws s3 rm s3://$S3_HOST_BUCKET_NAME--$AWS_ACCOUNT_ID --recursive --profile=${AWS_FRONTEND_PROFILE}"
+    CLEAR_COMMAND="aws s3 rm s3://$S3_HOST_BUCKET_NAME--$AWS_ACCOUNT_ID --recursive $PROFILE_COMMAND"
     echo $CLEAR_COMMAND;
     $CLEAR_COMMAND
 
     echo "Copy files..."
-    COPY_INDEX_COMMAND="aws s3 cp $DIR/../build/dist/index.html s3://$S3_HOST_BUCKET_NAME--$AWS_ACCOUNT_ID/ --cache-control 'no-cache' --profile=${AWS_FRONTEND_PROFILE}"
-    COPY_COMMAND="aws s3 cp $DIR/../build/dist/ s3://$S3_HOST_BUCKET_NAME--$AWS_ACCOUNT_ID/ --cache-control 'max-age=31536000' --exclude index.html --recursive  --profile=${AWS_FRONTEND_PROFILE}"
+    COPY_INDEX_COMMAND="aws s3 cp $DIR/../build/dist/index.html s3://$S3_HOST_BUCKET_NAME--$AWS_ACCOUNT_ID/ --cache-control 'no-cache' $PROFILE_COMMAND"
+    COPY_COMMAND="aws s3 cp $DIR/../build/dist/ s3://$S3_HOST_BUCKET_NAME--$AWS_ACCOUNT_ID/ --cache-control 'max-age=31536000' --exclude index.html --recursive  $PROFILE_COMMAND"
 
     $COPY_COMMAND;
     $COPY_INDEX_COMMAND;
 
-    DISTRIBUTION_ID=$(aws cloudfront list-distributions --query "DistributionList.Items[*].{id:Id,origin:Origins.Items[0].Id}[?origin=='$S3_HOST_BUCKET_NAME'].id" --output text --profile=${AWS_FRONTEND_PROFILE});
-    INVALIDATION_RESULT=$(aws cloudfront create-invalidation --distribution-id ${DISTRIBUTION_ID} --paths "/*" --profile=${AWS_FRONTEND_PROFILE})
+    DISTRIBUTION_ID=$(aws cloudfront list-distributions --query "DistributionList.Items[*].{id:Id,origin:Origins.Items[0].Id}[?origin=='$S3_HOST_BUCKET_NAME'].id" --output text $PROFILE_COMMAND);
+    INVALIDATION_RESULT=$(aws cloudfront create-invalidation --distribution-id ${DISTRIBUTION_ID} --paths "/*" $PROFILE_COMMAND)
     echo "Invalidation result:"
     echo $INVALIDATION_RESULT;
     
